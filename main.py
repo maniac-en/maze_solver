43381cadc8af1741633896f2737f822a7ac43527
Shivam Mehta <sm.cse17@gmail.com>
2023-12-04 01:12:13 +0530

    Added ability to break walls, dynamic window size

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from maze import Maze
 
 
 def main() -> None:
-    win = Window(550, 550)
-
-    m = Maze(25, 25, 10, 10, 50, 50, win)
+    startx, starty = 25, 25
+    row, col = 15, 15
+    cellx, celly = 50, 50
+    height = 2 * startx + row * cellx
+    width = 2 * starty + col * celly
+    win = Window(height, width)
+    m = Maze(startx, starty, row, col, cellx, celly, win)
     m._break_entrance_and_exit()
+    m._break_walls_r(0, 0)
     win.wait_for_close()
 
 
--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 from cell import Cell
 from time import sleep
+import random
 
 
 class Maze():
@@ -35,11 +36,11 @@ class Maze():
                 self._draw_cell(cell, i, j)
         return
 
-    def _draw_cell(self, cell, i, j) -> None:
+    def _draw_cell(self, cell, i, j, fill_color="black") -> None:
         x = self.x1 + i * self.cell_size_x
         y = self.y1 + j * self.cell_size_y
 
-        cell.draw(x, y, x + self.cell_size_x, y + self.cell_size_y)
+        cell.draw(x, y, x + self.cell_size_x, y + self.cell_size_y, fill_color)
         self._animate()
         return
 
@@ -51,8 +52,58 @@ class Maze():
                         len(self._cells[0]) - 1)
         return
 
+    # @@@ test case pending
+    def _get_neighbours(self, i, j) -> list:
+        neighbours = []
+        if i != 0:
+            neighbours.append((i - 1, j))
+        if j != 0:
+            neighbours.append((i, j - 1))
+        if i < len(self._cells) - 1:
+            neighbours.append((i + 1, j))
+        if j < len(self._cells[0]) - 1:
+            neighbours.append((i, j + 1))
+        return neighbours
+
+    # @@@ test case pending
+    def _break_walls_r(self, i, j) -> None:
+        self._cells[i][j].visited = True
+        while not self._cells[-1][-1].visited:
+            possible_directions = [d for d in filter(
+                lambda c: not self._cells[c[0]][c[1]].visited,
+                self._get_neighbours(i, j))]
+            if len(possible_directions) == 0:
+                self._draw_cell(self._cells[i][j], i, j)
+                i, j = random.choice(self._get_neighbours(i, j))
+            else:
+                to_i, to_j = random.choice(possible_directions)
+                if j == to_j:  # same row movement
+                    if to_i > i:
+                        self._cells[i][j].has_right_wall = False
+                        self._cells[to_i][to_j].has_left_wall = False
+                    else:
+                        self._cells[i][j].has_left_wall = False
+                        self._cells[to_i][to_j].has_right_wall = False
+                elif i == to_i:  # same column movement
+                    if to_j > j:
+                        self._cells[i][j].has_bottom_wall = False
+                        self._cells[to_i][to_j].has_top_wall = False
+                    else:
+                        self._cells[i][j].has_top_wall = False
+                        self._cells[to_i][to_j].has_bottom_wall = False
+
+                self._draw_cell(self._cells[i][j], i, j)
+                self._draw_cell(self._cells[to_i][to_j], to_i, to_j)
+                return self._break_walls_r(to_i, to_j)
+
+    # @@@ test case pending
+    def _reset_cells_visited(self) -> None:
+        for row in self._cells:
+            for cell in row:
+                cell.visited = False
+
     def _animate(self) -> None:
         if self._win:
             self._win.redraw()
-            # sleep(0.05)
+            sleep(0.02)
         return
